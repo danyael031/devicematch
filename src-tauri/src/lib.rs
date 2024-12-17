ba122dcd362aaf7b5a5e91279b60a670b5e26ba2
Dan Yael Sajarópulos Verdugo <danyael031@gmail.com>
2024-12-16 21:51:41 -0700

    Fixing database migration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -6,8 +9,52 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![Migration {
+        version: 1,
+        description: "create_initial_tables",
+        sql: "
+CREATE TABLE brands (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    image BLOB
+);
+
+CREATE TABLE devices (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    image BLOB,
+    brand_id TEXT,
+    FOREIGN KEY (brand_id) REFERENCES brands(id)
+);
+
+CREATE TABLE categories (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    image BLOB
+);
+
+CREATE TABLE device_compatibility (
+    device1_id TEXT NOT NULL,
+    device2_id TEXT NOT NULL,
+    category_id TEXT NOT NULL,
+    score INTEGER NOT NULL,
+    UNIQUE(device1_id, device2_id, category_id),
+    CHECK (device1_id < device2_id),
+    CHECK (score BETWEEN 0 AND 5),
+    FOREIGN KEY (device1_id) REFERENCES devices(id),
+    FOREIGN KEY (device2_id) REFERENCES devices(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+",
+        kind: MigrationKind::Up,
+    }];
+
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(
+            tauri_plugin_sql::Builder::new()
+                .add_migrations("sqlite:phonecases.db", migrations)
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,7 @@
 import Button from '@mui/material/Button'
 import { createPhone, createTable, getPhones } from '../../db/phones'
+import { appDataDir } from '@tauri-apps/api/path';
+import { appConfigDir } from '@tauri-apps/api/path';
 
 
 
@@ -9,6 +11,13 @@ export function MainPage() {
     const response = await getPhones();
     console.log(response);
   }
+  async function printAppPath() {
+    const appDataDirPath = await appDataDir();
+    const appConfigDirPath = await appConfigDir();
+    console.log("appDataDir: ", appDataDirPath)
+    console.log("appConfigDir: ", appConfigDirPath)
+  }
+
   return (
     <div>
       <Button
@@ -23,6 +32,10 @@ export function MainPage() {
         onClick={handleGetPhones}
         variant='contained'
       >Get Phones</Button>
+      <Button
+        onClick={printAppPath}
+        variant='contained'
+      >Print app data dir</Button>
     </div>
   )
 }
